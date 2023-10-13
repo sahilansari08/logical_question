d22ba0be19819ad5693a65ee03a9d630ce93809e
sahilansari08 <sahilansaria453@gmail.com>
2023-10-13 18:13:49 +0530

    second

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+const { logger } = require("prompt")
+
 const list = ["a","b","c","d","e","f"]
 // original = [a,b,c,d,e,f]
 // second = [a,b,c,d,e,f]
 list.shift()
 const list2 = list 
 console.log(list2)
+
+///
+const list1 = ["a","b","c","d","e","f"]
+// original = [a,b,c,d,e,f]
+// second = [a,b,c,d,e,f]
+list.shift()
+const list3 = list1 
+console.log(list3)
+console.log("kndjkhgoiejrgp");
+
